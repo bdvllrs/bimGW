a96e0d9be60b2b54fc0a1dcfca08349d46f9dc0f
Benjamin <bdvllrs@gmail.com>
2021-09-17 15:16:14 +0200

    Remove imagenet download in slurm script.

--- submitSLURMScript.sh
+++ submitSLURMScript.sh
@@ -6,11 +6,11 @@ scriptName=$2
 
 # Copy dataset to tmpdir
 
-if [[ ! -e "${DATASET_FOLDER}/imagenet" ]]; then
-  if [[ -e "${WORK_DATASET_FOLDER}/imagenet" ]]; then
-    echo "Copying dataset"
-    cp -r "${WORK_DATASET_FOLDER}/imagenet" "${DATASET_FOLDER}/imagenet"
-  fi
-fi
+#if [[ ! -e "${DATASET_FOLDER}/imagenet" ]]; then
+#  if [[ -e "${WORK_DATASET_FOLDER}/imagenet" ]]; then
+#    echo "Copying dataset"
+#    cp -r "${WORK_DATASET_FOLDER}/imagenet" "${DATASET_FOLDER}/imagenet"
+#  fi
+#fi
 
 bash ~/submitJob.sh "${BIM_ENV}" "${jobName}" "${BIM_WORKDIR}" "${RUN_WORKDIR}/BIM" "bimGW/scripts/${scriptName}.py" "${@:3}"
