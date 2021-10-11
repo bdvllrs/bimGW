Fix rotations in degrees when should be in radians.

bim_gw/modules/gw.py
@@ -34,9 +34,6 @@ class DomainDecoder(torch.nn.Module):
             nn.Linear(self.in_dim, self.hidden_size),
             nn.BatchNorm1d(self.hidden_size),
             nn.ReLU(),
-            nn.Linear(self.hidden_size, self.hidden_size),
-            nn.BatchNorm1d(self.hidden_size),
-            nn.ReLU(),
         )
 
         self.encoder_head = nn.ModuleList([
@@ -70,9 +67,6 @@ class DomainEncoder(nn.Module):
 
         self.encoder = nn.Sequential(
             nn.Linear(sum(self.in_dims), self.hidden_size),
-            nn.BatchNorm1d(self.hidden_size),
-            nn.ReLU(),
-            nn.Linear(self.hidden_size, self.hidden_size),
             nn.BatchNorm1d(self.hidden_size),
             nn.ReLU(),
             nn.Linear(self.hidden_size, self.out_dim),
@@ -379,6 +373,17 @@ class GlobalWorkspace(LightningModule):
                 for domain_name, domain_example in examples.items():
                     self.domain_mods[domain_name].log_domain(self.logger, domain_example,
                                                              f"{slug}_original_domain_{domain_name}", max_examples)
+
+                    if domain_name == "v":
+                        latent = self.domain_mods[domain_name].encode(domain_example)
+                        fig, axes = plt.subplots(1, latent.size(1))
+                        for k in range(latent.size(1)):
+                            l = latent.detach().cpu().numpy()[:, k]
+                            x = np.linspace(-0.8, 0.8, 100)
+                            axes[k].hist(l, 50, density=True)
+                            axes[k].plot(x, scipy.stats.norm.pdf(x, 0, 1))
+                        self.logger.experiment["original_v_hist"].log(File.as_image(fig))
+                        plt.close(fig)
 
             for domain_name, domain_example in examples.items():
                 # Demi cycles

bim_gw/modules/language_model.py
@@ -96,7 +96,7 @@ class ShapesLM(WorkspaceModule):
         out_latents = latent.clone()
         out_latents[:, 0] = latent[:, 0] * self.imsize
         out_latents[:, 1] = latent[:, 1] * self.imsize
-        # out_latents[:, 2] = latent[:, 2] * self.imsize
+        out_latents[:, 2] = latent[:, 2] * self.imsize
         return (torch.argmax(logits, dim=-1),
                 out_latents)
 
@@ -105,7 +105,7 @@ class ShapesLM(WorkspaceModule):
         out_latents = latents.clone()
         out_latents[:, 0] = latents[:, 0] / self.imsize
         out_latents[:, 1] = latents[:, 1] / self.imsize
-        # out_latents[:, 2] = latents[:, 2] / self.imsize
+        out_latents[:, 2] = latents[:, 2] / self.imsize
         return (torch.nn.functional.one_hot(cls, self.n_classes).type_as(latents),
                 # rotations,
                 out_latents)

bim_gw/utils/shapes.py
@@ -30,11 +30,13 @@ def get_square_patch(location, radius, rotation, color):
 
 def get_triangle_patch(location, radius, rotation, color):
     x, y = location[0], location[1]
-    coordinates = np.array([[-radius, 0],
-                            [radius, 0],
-                            [radius, -radius]])
+    coordinates = np.array([[-radius / 2, -radius / 2],
+                            [radius / 2, -radius / 2],
+                            [radius / 2, radius]])
     center = np.mean(coordinates, axis=0)
+    # center = np.array([[0, 0]])
     origin = np.array([[x, y]])
+    # rotation = rotation - np.pi / 2
     rotation_m = np.array([[np.cos(rotation), -np.sin(rotation)], [np.sin(rotation), np.cos(rotation)]])
     patch = patches.Polygon(origin + center + (coordinates - center) @ rotation_m.T, facecolor=color)
     return patch
@@ -56,7 +58,7 @@ def get_egg_patch(location, radius, rotation, color):
                             [-0.8, -0.5],
                             [0, -0.5]])
     scale = radius * 1.4
-    center = np.array([[0.5, 0.5]])
+    center = np.array([[0, 0]])
     origin = np.array([[x, y]])
     rotation_m = np.array([[np.cos(rotation), -np.sin(rotation)], [np.sin(rotation), np.cos(rotation)]])
     codes = [mpath.Path.MOVETO, mpath.Path.CURVE4, mpath.Path.CURVE4, mpath.Path.CURVE4,
@@ -138,9 +140,10 @@ def log_shape_fig(logger, classes, latents, name):
     plt.close(fig)
 
 
-def generate_radius(n_samples, min, max):
-    assert max > min
-    return np.random.randint(min, max, n_samples)
+def generate_radius(n_samples, min_val, max_val):
+    assert max_val > min_val
+    # return np.random.randint(min_val, max_val, n_samples)
+    return np.full(n_samples, max_val)
 
 
 def generate_color(n_samples, min_lightness=0, max_lightness=256):
@@ -153,7 +156,8 @@ def generate_color(n_samples, min_lightness=0, max_lightness=256):
 
 
 def generate_rotation(n_samples, classes):
-    rotations = np.random.rand(n_samples) * 360
+    # rotations = np.random.rand(n_samples) * 2 * np.pi
+    rotations = np.full(n_samples, 0)
     # rotations[classes == 1] = 0  # circles don't have rotations
     # rotations[classes == 0] = rotations[classes == 0] % 90
     return rotations
@@ -163,6 +167,7 @@ def generate_location(n_samples, radius, imsize):
     assert (radius <= imsize / (2 * np.sqrt(2))).all()
     radii = np.sqrt(2) * np.stack((radius, radius), axis=1)
     locations = np.random.randint(radii, imsize - radii, (n_samples, 2))
+    # locations = np.full((n_samples, 2), imsize // 2)
     return locations
 
 

scripts/toy_dataset.py
@@ -37,14 +37,14 @@ def save_labels(path_root, dataset):
 def main():
     seed = 0
     image_size = 32
-    dataset_location = Path("/mnt/SSD/datasets/shapes_v5")
-    size_train_set = 500_000
-    size_val_set = 50_000
-    size_test_set = 50_000
+    dataset_location = Path("/mnt/SSD/datasets/shapes_v6")
+    size_train_set = 50_000
+    size_val_set = 5_000
+    size_test_set = 5_000
 
     # in pixels
     min_radius = 5
-    max_radius = 11
+    max_radius = 10
     min_lightness = 46  # of the HSL format. Higher value generates lighter images. Max is 256
     max_lightness = 256  # of the HSL format. Higher value generates lighter images. Max is 256
     class_names = np.array(["square", "circle", "triangle"])