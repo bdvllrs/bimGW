c0643d9f1c4db33e91507831a43b038a1773a1ad
Benjamin <bdvllrs@gmail.com>
2022-03-14 13:56:30 +0100

    Refactoring.

--- scripts/analyze_generalization.py
+++ scripts/analyze_generalization.py
@@ -7,10 +7,12 @@ if __name__ == '__main__':
     log = CSVLog("../data/bim-gw.csv")
     # log = log.add_token_column().filter_between("_token", 910, 958)  # attributes
     # log = log.add_token_column().filter_between("_token", 959, 1007)  # BERT vectors
-    log = log.add_token_column().filter_between("_token", 1120, 1189)  # BERT vectors
+    log = log.add_token_column().filter_between("_token", 1273, 1357)  # BERT vectors
     log = log.filter_neq("prop_lab_images", "")
     log = log.add_column("n_images", lambda row: int(row["prop_lab_images"] * 500_000))
-    log = log.filter_eq("parameters/seed", 0)
+    # log = log.filter_eq("parameters/seed", 0)
+    seeds = [0, 1, 2, 3, 4]
+    log = log.filter_in("parameters/seed", seeds)
 
     n_images = sorted(np.unique(log.values("n_images")))
 
@@ -20,18 +22,24 @@ if __name__ == '__main__':
                 x = []
                 y = []
                 err = []
+                n_runs = []
                 for n_image in n_images:
                     l = log.filter_eq("n_images", n_image)
                     l = l.filter_eq("parameters/losses/coefs/cycles", use_cycles)
                     x.append(n_image)
-                    values = np.array(l.values(f'metrics/val_{type_}_supervision_{loss} (last)'))
+                    values = np.array(l.values(f'training/val_{type_}_supervision_{loss} (last)'))
+                    # if values.shape[0] != len(seeds):
+                    #     print("nooo")
+                    # assert values.shape[0] == len(seeds)
+                    print(values.shape[0], n_image / 500_000)
+                    n_runs.append(values.shape[0])
                     y.append(np.mean(values))
-                    # err.append(np.std(values))
+                    err.append(np.std(values))
 
-                # plt.errorbar(x, y, yerr=np.array(err) / np.sqrt(5), label=f"{type_} {supervision}")
-                plt.plot(x, y, label=f"{type_} {supervision}")
+                plt.errorbar(x, y, yerr=np.divide(np.array(err), np.sqrt(n_runs)), label=f"{type_} {supervision}")
+                # plt.plot(x, y, label=f"{type_} {supervision}")
 
-        # plt.gca().set_yscale("log")
+        plt.gca().set_yscale("log")
         plt.gca().set_xscale("log")
         plt.xlabel("Number of synchronized images")
         plt.ylabel(f"AVG translation validation losses")
