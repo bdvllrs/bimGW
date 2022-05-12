a258b038e683a28830fae4aa0cdd32ca557ea13f
Benjamin <bdvllrs@gmail.com>
2022-05-12 10:43:43 +0200

    Resolve args before sending to auto-sbatch

--- slurm/train.py
+++ slurm/train.py
@@ -9,8 +9,9 @@ if __name__ == '__main__':
     work_dir = Path(__file__).parent.parent
     script_location = work_dir.name + "scripts/train.py"
 
-    args = get_args(debug=int(os.getenv("DEBUG", 0))).slurm
-    args = OmegaConf.merge(args, {
+    args = get_args(debug=int(os.getenv("DEBUG", 0)))
+    OmegaConf.resolve(args)
+    args = OmegaConf.merge(args.slurm, {
         "work_directory": str(work_dir.absolute()),
         "script_location": script_location
     })
