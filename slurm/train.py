3f51eb567b1289f889b1afb659d2ef8809319234
Benjamin <bdvllrs@gmail.com>
2022-05-12 17:14:17 +0200

    Update script location to fit with new auto_sbatch version

--- slurm/train.py
+++ slurm/train.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from auto_sbatch import auto_sbatch
+from auto_sbatch import SBatch, ExperimentHandler
 from omegaconf import OmegaConf
 
 from bim_gw.utils import get_args
@@ -16,5 +16,20 @@ if __name__ == '__main__':
         "script_location": script_location
     })
 
-    auto_sbatch(args)
+    handler = ExperimentHandler(
+        script_location,
+        str(work_dir.absolute()),
+        args.slurm.run_work_directory,
+        args.slurm.python_environment,
+        args.slurm.run_registry_path,
+        pre_modules=["python/3.8.5"],
+        run_modules=["python/3.8.5", "cuda/11.5"],
+        additional_scripts=[
+            "pip install torch==1.11.0+cu115 torchvision==0.12.0+cu115 -f https://download.pytorch.org/whl/torch_stable.html"
+        ]
+    )
+
+    sbatch = SBatch(args, handler)
+    sbatch.add_command('export WANDB_MODE="offline"')
+    sbatch()
 
