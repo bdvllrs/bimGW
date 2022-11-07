7371fdb1ff55caa6785b3feed61cb2b86ff1330e
Benjamin <bdvllrs@gmail.com>
2022-11-07 17:10:58 +0100

    Update odd_image_out to do transfer learning

--- bim_gw/datasets/odd_image/dataset.py
+++ bim_gw/datasets/odd_image/dataset.py
@@ -2,15 +2,16 @@ from pathlib import Path
 
 import numpy as np
 import torch
-from bim_gw.datasets.simple_shapes.fetchers import PreSavedLatentDataFetcher
+from bim_gw.datasets.simple_shapes.fetchers import PreSavedLatentDataFetcher, AttributesDataFetcher
 
 from bim_gw.datasets.pre_saved_latents import load_pre_saved_latent
 
 
 class OddImageDataset:
-    def __init__(self, root_path, split, pre_saved_latent_path):
+    def __init__(self, root_path, split, pre_saved_latent_path, selected_domains):
         self.root_path = Path(root_path)
         self.split = split
+        self.selected_domains = selected_domains
 
         self.labels = np.load(str(root_path / f"{split}_odd_image_labels.npy"))
 
@@ -20,19 +21,31 @@ class OddImageDataset:
         elif split == "test":
             self.shift_ref_item = 750_000
 
-
-        self.visual_domain = PreSavedLatentDataFetcher(
-            # split always train, we used the end 500_000 as val/test for this dataset.
-            load_pre_saved_latent(self.root_path, "train", pre_saved_latent_path, "v"))
+        ids = np.arange(1_000_000)
+        labels = np.load(str(self.root_path / f"train_labels.npy"))
+        fetchers = {
+            "v": PreSavedLatentDataFetcher(
+                # split always train, we used the end 500_000 as val/test for this dataset.
+                # We only used the 500 000 first examples for training the other models, we use the 500 000 unseen elements
+                # from the train set
+                load_pre_saved_latent(self.root_path, "train", pre_saved_latent_path, "v")),
+            "attr": AttributesDataFetcher(self.root_path, "train", ids, labels, {"attr": None}),
+            "t": PreSavedLatentDataFetcher(
+                load_pre_saved_latent(self.root_path, "train", pre_saved_latent_path, "t")),
+        }
+        self.fetchers = {name: fetchers[name] for name in selected_domains.values()}
 
     def __len__(self):
         return self.labels.shape[0]
 
     def __getitem__(self, item):
         label = self.labels[item]
-        return (
-            self.visual_domain.get_item(label[0] + self.shift_ref_item)[1],
-            self.visual_domain.get_item(label[1] + self.shift_ref_item)[1],
-            self.visual_domain.get_item(label[2] + self.shift_ref_item)[1],
-            torch.tensor(label[3], dtype=torch.long)
-        )
+        data = {
+            name: (self.fetchers[name].get_item(label[0] + self.shift_ref_item)[1:],
+                  self.fetchers[name].get_item(label[1] + self.shift_ref_item)[1:],
+                  self.fetchers[name].get_item(label[2] + self.shift_ref_item)[1:])
+            for name in self.fetchers.keys()
+        }
+        data["label"] = torch.tensor(label[3], dtype=torch.long)
+        return data
+
--- bim_gw/modules/odd_classifier.py
+++ bim_gw/modules/odd_classifier.py
@@ -5,11 +5,15 @@ from torch import nn
 
 
 class OddClassifier(LightningModule):
-    def __init__(self, encoder, z_size, optimizer_lr=1e-3, optimizer_weight_decay=1e-5):
+    def __init__(self, unimodal_encoders, encoders, z_size, optimizer_lr=1e-3, optimizer_weight_decay=1e-5):
         super(OddClassifier, self).__init__()
         self.save_hyperparameters(ignore=["encoder"])
-        self.encoder = encoder
+        self.unimodal_encoders = nn.ModuleDict(unimodal_encoders)
+        self.encoders = nn.ModuleDict(encoders)
         self.z_size = z_size
+
+        for mod in unimodal_encoders.values():
+            mod.freeze()  # insures that all modules are frozen
 
         self.train_acc = torchmetrics.Accuracy()
         self.val_acc = torchmetrics.Accuracy()
@@ -21,19 +25,30 @@ class OddClassifier(LightningModule):
         )
 
     def step(self, batch, mode="train"):
-        latents = torch.cat([
-            self.encoder([batch[0]]),
-            self.encoder([batch[1]]),
-            self.encoder([batch[2]]),
-        ], dim=1)
+        latents = {
+            name: torch.cat([
+                    self.encoders[name](self.unimodal_encoders[name](batch[name][0])),
+                    self.encoders[name](self.unimodal_encoders[name](batch[name][1])),
+                    self.encoders[name](self.unimodal_encoders[name](batch[name][2])),
+                ], dim=1)
+            for name in self.unimodal_encoders.keys()
+        }
 
-        prediction = self.classifier(latents)
-        loss = torch.nn.functional.cross_entropy(prediction, batch[3])
-        self.log(f"{mode}_loss", loss)
-        acc_fn = self.train_acc if mode == "train" else self.val_acc
-        acc_fn(prediction.softmax(-1), batch[3])
-        self.log(f"{mode}_acc", acc_fn, on_epoch=(mode=="val"))
-        return loss
+        predictions = {
+            name: self.classifier(latents[name])
+            for name in self.unimodal_encoders.keys()
+        }
+        losses = {
+            name: torch.nn.functional.cross_entropy(predictions[name], batch['label'])
+            for name in self.unimodal_encoders.keys()
+        }
+        for name in losses.keys():
+            self.log(f"{mode}_{name}_loss", losses[name])
+            acc_fn = self.train_acc if mode == "train" else self.val_acc
+            acc_fn(predictions[name].softmax(-1), batch['label'])
+            self.log(f"{mode}_acc", acc_fn, on_epoch=(mode=="val"))
+        self.log(f"{mode}_loss", losses["v"])
+        return losses["v"]
 
     def training_step(self, batch, batch_idx):
         loss = self.step(batch, mode="train")
