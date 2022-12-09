36c315cad16395ceb9f0ef30f917d8c98239f81e
Benjamin <bdvllrs@gmail.com>
2022-12-09 17:11:45 +0100

    More various captions.

--- bim_gw/utils/text_composer/composer.py
+++ bim_gw/utils/text_composer/composer.py
@@ -5,6 +5,17 @@ from attributes_to_language.composer import Composer
 from bim_gw.utils.text_composer.modifiers import MixModifier, DeleteModifier
 from bim_gw.utils.text_composer.writers import writers
 
+
+def a_has_n(sentence):
+    def aux(attributes):
+        vowels = ["a", "e", "i", "o", "u"]
+        if attributes[attributes['_next']][0] in vowels:
+            return sentence.format(**{"n?": "n"})
+        return sentence.format(**{"n?": ""})
+
+    return aux
+
+
 # or from a kwargs given to the composer method.
 script_structures = [
     "{start} {size}{link}{color}{link}{shape}{link}{location}{link}{rotation}.",
@@ -27,33 +38,46 @@ modifiers = None
 #     MixModifier(0.2)
 # ]
 
-composer = Composer(script_structures, variants, writers, modifiers)
+composer = Composer(script_structures, writers, variants, modifiers)
 
 script_structures = [
-    "{start} {size} {color} {shape}, {location}{link2} {rotation}.",
-    "{start} {color} {size} {shape}, {location}{link2} {rotation}.",
-    "{start} {size} {shape} in {color} color, {location}{link2} {rotation}.",
-    "{start} {size} {shape} in {color} color{link} {location}{link2} {rotation}.",
-    "{start} {size} {color} {shape}{link} {location}{link2} {rotation}.",
-    "{start} {color} {size} {shape}{link} {location}{link2} {rotation}.",
+    "{start} {size} {colorBefore} {shape}{link} <{location}>{link} <{rotation}>.",
+    "{start} {size} {colorBefore} {object} <{location}>{link} <{rotation}>{link2} {a} {shape}.",
+    "{start} {size} {object} <{location}>{link} <{rotation}>{link} {colorBoth}{link2} {a} {shape}.",
+    "{start} {size} {object} in {colorAfter} <{location}>{link} <{rotation}>{link2} {a} {shape}.",
+    "{start} {shape}{link} {size}{link3} {colorBoth}{link} <{location}>{link} <{rotation}>.",
+    "{start} {color}{colored?} {size} {shape}{link} <{location}>{link} <{rotation}>.",
+    "{start} {color}{colored?} {shape}{link} <{size}>{link} <{location}>{link} <{rotation}>.",
 ]
+
+start_variant = ["A", "It is a", "This is a", "There is a",
+                 "The image is a", "The image represents a", "The image contains a"]
+start_variant = [a_has_n(x + "{n?}") for x in start_variant] + ['A kind of']
 
 # Elements in the list of each variant is randomly chosen.
 variants = {
-    "start": ["A", "It is a", "A kind of", "This is a", "There is a",
-              "The image is a", "The image represents a", "The image contains a"],
-    "link": [". It is", ", it is", ", and is"],
-    "link2": [", and is", ", and", ". It is", ", it is"]
+    "start": start_variant,
+    "a": [a_has_n("a{n?}")],
+    "object": ["shape", "object"],
+    "colored?": ["", " colored"],
+    "colorBefore": ["{color}", "{color} colored"],
+    "colorAfter": ["{color}", "{color} color"],
+    "colorBoth": ["{color}", "in {color}", "{color} colored", "in {color} color"],
+    ",?": [", ", " "],
+    "link": [". It is", ", it is", "{,?}and is", "{,?}and it is", ",",],
+    "link2": [". It looks like", ", it looks like", "{,?}and looks like", "{,?}and it looks like"],
+    "link3": [". It is", ", it is", "{,?}and is", "{,?}and it is", ",", " and"],
 }
 
-val_composer = Composer(script_structures, variants, writers, modifiers)
+val_composer = Composer(script_structures, writers, variants, modifiers)
 
 if __name__ == '__main__':
-    for k in range(5):
-        print(composer({
-            "shape": 2,
-            "rotation": np.pi/6,
-            "color": (129, 76, 200),
-            "size": 20,
-            "location": (29, 8)
+    labels = np.load(str("/mnt/SSD/datasets/shapes_v13/train_labels.npy"))
+    for k in range(100):
+        print(val_composer({
+            "shape": int(labels[k][0]),
+            "rotation": labels[k][4],
+            "color": (labels[k][5], labels[k][6], labels[k][7]),
+            "size": labels[k][3] // 4,
+            "location": (labels[k][1] // 4, labels[k][2] // 4)
         }))
