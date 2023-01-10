337ff4bc43febdac1509a0b1cef82cf9e7b4b209
Benjamin <bdvllrs@gmail.com>
2023-01-10 16:12:11 +0100

    Update requirements.txt

--- setup.py
+++ setup.py
@@ -6,15 +6,24 @@ def get_requirements():
         requirements = f.read().splitlines()
     return requirements
 
+
 def get_version():
     with open("version.txt", "r") as f:
         version = f.read().lstrip("\n")
     return version
 
 
+extras_requires = {
+    "wandb": ["wandb>=0.13.4"],
+    "neptune": ["neptune-client>=0.16"],
+}
+extras_requires["all"] = list(set(sum(extras_requires.values(), [])))
+
 if __name__ == '__main__':
-    setup(name='bim_gw',
-          version=get_version(),
-          install_requires=get_requirements(),
-          packages=find_packages(),
-          )
+    setup(
+        name='bim_gw',
+        version=get_version(),
+        install_requires=get_requirements(),
+        packages=find_packages(),
+        extras_requires=extras_requires
+    )
