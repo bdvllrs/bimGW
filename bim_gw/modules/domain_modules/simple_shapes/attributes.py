6a91fcf4d785958a74485d9fb58bea8c88594590
Benjamin <bdvllrs@gmail.com>
2023-01-26 14:59:27 +0100

    Set scaling to 100.

--- bim_gw/modules/domain_modules/simple_shapes/attributes.py
+++ bim_gw/modules/domain_modules/simple_shapes/attributes.py
@@ -43,7 +43,7 @@ class SimpleShapesAttributes(DomainModule):
         out_latents[:, 2] = out_latents[:, 2] / self.imsize
         out_latents = out_latents * 2 - 1
         if self.use_unpaired:  # HUGE scaling for testing
-            out_latents[:, -1] = out_latents[:, -1] * 50
+            out_latents[:, -1] = out_latents[:, -1] * 100
         return (
             torch.nn.functional.one_hot(cls, self.n_classes).type_as(latents),
             out_latents,
@@ -53,7 +53,7 @@ class SimpleShapesAttributes(DomainModule):
         logits, latents = x
         out_latents = latents.clone()
         if self.use_unpaired:  # HUGE scaling for testing
-            out_latents[:, -1] = out_latents[:, -1] / 50
+            out_latents[:, -1] = out_latents[:, -1] / 100
         out_latents = (out_latents + 1) / 2
         out_latents[:, 0] = out_latents[:, 0] * self.imsize
         out_latents[:, 1] = out_latents[:, 1] * self.imsize
