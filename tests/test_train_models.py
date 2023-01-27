8311c5db3e72556d93e1e85ba112cb8ccf047e56
Benjamin <bdvllrs@gmail.com>
2023-01-27 15:57:17 +0100

    Fix already existing tests.

--- tests/test_train_models.py
+++ tests/test_train_models.py
@@ -3,7 +3,7 @@
 import os
 from pathlib import Path
 
-from bim_gw.scripts.train import train_gw, train_lm, train_vae
+from bim_gw.scripts.train import train_gw, train_vae, train_lm
 from bim_gw.utils import get_args
 
 
@@ -24,6 +24,7 @@ def test_train_lm():
 def test_train_gw_with_attributes():
     args = get_args(debug=int(os.getenv("DEBUG", 0)), additional_config_files=[
         Path("configs/test_base.yaml"),
+        Path("configs/load_pretrained_models.yaml"),
         Path("configs/test_gw_with_attributes.yaml")
     ])
     train_gw(args)
@@ -32,6 +33,7 @@ def test_train_gw_with_attributes():
 def test_train_gw_with_text():
     args = get_args(debug=int(os.getenv("DEBUG", 0)), additional_config_files=[
         Path("configs/test_base.yaml"),
+        Path("configs/load_pretrained_models.yaml"),
         Path("configs/test_gw_with_text.yaml")
     ])
     train_gw(args)
@@ -40,6 +42,7 @@ def test_train_gw_with_text():
 def test_train_gw_with_unsync_examples():
     args = get_args(debug=int(os.getenv("DEBUG", 0)), additional_config_files=[
         Path("configs/test_base.yaml"),
+        Path("configs/load_pretrained_models.yaml"),
         Path("configs/test_gw_with_unsync_examples.yaml")
     ])
     train_gw(args)
@@ -48,6 +51,7 @@ def test_train_gw_with_unsync_examples():
 def test_train_gw_with_split_ood():
     args = get_args(debug=int(os.getenv("DEBUG", 0)), additional_config_files=[
         Path("configs/test_base.yaml"),
+        Path("configs/load_pretrained_models.yaml"),
         Path("configs/test_gw_with_split_ood.yaml")
     ])
     train_gw(args)
