b11bfa8b555e99f904afd4c2572d64d88bb732de
Benjamin <bdvllrs@gmail.com>
2023-03-17 14:30:30 +0100

    Add support for optional, union and multiple cli_names.

--- bim_gw/utils/cli.py
+++ bim_gw/utils/cli.py
@@ -6,12 +6,21 @@ import yaml
 
 
 def _get_real_field(fields, name):
-    if name in fields.keys():
-        return fields[name]
     for field in fields.values():
-        if field.metadata.get("cli", field.name) == name:
+        if name in field.metadata.get("cli_names", [field.name]):
             return field
     return None
+
+
+def _isinstance(obj, type_):
+    try:
+        subtypes = type_.__args__
+        for subtype in subtypes:
+            if isinstance(obj, subtype):
+                return True
+        return False
+    except AttributeError:
+        return isinstance(obj, type_)
 
 
 def _is_valid_dataclass_type(dataclass_type, loaded_value):
@@ -23,7 +32,7 @@ def _is_valid_dataclass_type(dataclass_type, loaded_value):
         real_field = _get_real_field(sub_fields, val_name)
         if real_field is None:
             return False
-        if not _is_valid_field(real_field.type, sub_val):
+        if not _is_valid_field_from_type(real_field.type, sub_val):
             return False
     return True
 
@@ -33,36 +42,36 @@ def _is_valid_list(field_type, loaded_value):
         return False
     for val in loaded_value:
         field_type = field_type.__args__[0]
-        if not _is_valid_field(field_type, val):
+        if not _is_valid_field_from_type(field_type, val):
             return False
     return True
 
 
-def _is_valid_field(field_type, loaded_value):
+def _is_valid_field_from_type(field_type, loaded_value):
     if isinstance(loaded_value, (list, tuple)):
         return _is_valid_list(field_type, loaded_value)
-    if is_dataclass(field_type):
+    if _is_dataclass(field_type):
         return _is_valid_dataclass_type(field_type, loaded_value)
-    return isinstance(loaded_value, field_type)
+    return _isinstance(loaded_value, field_type)
 
 
-def is_valid_field(field, value):
+def _is_valid_field(field, value):
     loaded_value = yaml.load(value, Loader=yaml.SafeLoader)
-    return _is_valid_field(field.type, loaded_value)
+    return _is_valid_field_from_type(field.type, loaded_value)
 
 
-def is_field_flag(field):
+def _is_field_flag(field):
     return field.type is bool
 
 
-def is_dataclass(structure):
+def _is_dataclass(structure):
     return (
             dataclasses.is_dataclass(structure)
             and isinstance(structure, type)
     )
 
 
-def split_argv(argv):
+def _split_argv(argv):
     new_argv = []
     for arg in argv:
         if "=" in arg:
@@ -72,7 +81,7 @@ def split_argv(argv):
     return new_argv
 
 
-def get_field(structure, key: str):
+def _get_field(structure, key: str):
     key_parts = key.split(".")
     fields = {field.name: field for field in dataclasses.fields(structure)}
     field = None
@@ -81,7 +90,7 @@ def get_field(structure, key: str):
         if field is None:
             return None
 
-        if is_dataclass(field.type):
+        if _is_dataclass(field.type):
             structure = field.type
             fields = {field.name: field
                       for field in dataclasses.fields(structure)}
@@ -92,32 +101,32 @@ def parse_argv_from_structure(
     structure,
     argv: Optional[List[str]] = None,
 ):
-    if not is_dataclass(structure):
+    if not _is_dataclass(structure):
         raise TypeError("Structure must be a dataclass")
 
     if argv is None:
         argv = sys.argv[1:]
     dotlist = []
     # remove all = and split keys and values
-    argv = split_argv(argv)
+    argv = _split_argv(argv)
 
     last_key = None
     last_field = None
     for arg in argv:
-        arg_field = get_field(structure, arg)
+        arg_field = _get_field(structure, arg)
         is_arg_valid_key = arg_field is not None
 
         # Last key was a flag
         if (
                 is_arg_valid_key
                 and last_field is not None
-                and is_field_flag(structure, last_field)
+                and _is_field_flag(last_field)
         ):
             dotlist.append(f"{last_key}=True")
             last_key, last_field = None, None
 
         # This arg is a value
-        if last_key is not None and is_valid_field(last_field, arg):
+        if last_key is not None and _is_valid_field(last_field, arg):
             dotlist.append(f"{last_key}={arg}")
             last_key, last_field = None, None
             continue
@@ -131,7 +140,7 @@ def parse_argv_from_structure(
 
     # Last key was a flag
     if (last_field is not None
-            and is_field_flag(last_field)):
+            and _is_field_flag(last_field)):
         dotlist.append(f"{last_key}=True")
 
     return dotlist
--- tests/utils/test_config.py
+++ tests/utils/test_config.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional, Union
 
 import pytest
 from omegaconf import MISSING
@@ -18,11 +18,13 @@ class StructuredConfig:
     param2: bool = True
     param3: str = MISSING
     param4: NestedConfig = field(default_factory=NestedConfig)
-    param5: str = field(default="nok", metadata={"cli": "--param5"})
+    param5: str = field(default="nok", metadata={"cli_names": ["--param5"]})
     param6: List[NestedConfig] = field(
         default_factory=lambda: [NestedConfig(param4_1={})]
     )
     param7: Any = "ok"
+    param8: Optional[int] = None
+    param9: Union[float, int] = field(default=42)
 
 
 def test_parse_argv_from_structure():
@@ -34,6 +36,14 @@ def test_parse_argv_from_structure():
 
 
 def test_parse_argv_from_structure_with_flag():
+    dotlist = parse_argv_from_structure(
+        StructuredConfig,
+        ["param2", "param1=2"]
+    )
+    assert dotlist == ["param2=True", "param1=2"]
+
+
+def test_parse_argv_from_structure_with_flag_last():
     dotlist = parse_argv_from_structure(
         StructuredConfig,
         ["param2"]
@@ -103,3 +113,51 @@ def test_parse_argv_from_structure_with_metadata():
         ["--param5", "ok"]
     )
     assert dotlist == ["--param5=ok"]
+
+
+def test_parse_argv_from_structure_with_optional():
+    dotlist = parse_argv_from_structure(
+        StructuredConfig,
+        ["param8=1"]
+    )
+    assert dotlist == ["param8=1"]
+
+
+def test_parse_argv_from_structure_with_optional_null():
+    dotlist = parse_argv_from_structure(
+        StructuredConfig,
+        ["param8=null"]
+    )
+    assert dotlist == ["param8=null"]
+
+
+def test_parse_argv_from_structure_with_optional_null_fail():
+    with pytest.raises(ValueError):
+        parse_argv_from_structure(
+            StructuredConfig,
+            ["param8=nok"]
+        )
+
+
+def test_parse_argv_from_structure_with_union_type_1():
+    dotlist = parse_argv_from_structure(
+        StructuredConfig,
+        ["param9=0.3"]
+    )
+    assert dotlist == ["param9=0.3"]
+
+
+def test_parse_argv_from_structure_with_union_type_2():
+    dotlist = parse_argv_from_structure(
+        StructuredConfig,
+        ["param9=3"]
+    )
+    assert dotlist == ["param9=3"]
+
+
+def test_parse_argv_from_structure_with_union_fail():
+    with pytest.raises(ValueError):
+        parse_argv_from_structure(
+            StructuredConfig,
+            ["param9=nok"]
+        )
