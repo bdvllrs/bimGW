06396a442aca56ea918c0250808d5b54b4e79167
Benjamin <bdvllrs@gmail.com>
2023-03-22 14:22:03 +0100

    Add tests to load dataset.

---
+++ tests/datasets/test_dataset_utils.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+from bim_gw.datasets.utils import load_simple_shapes_dataset
+from bim_gw.utils import get_args
+
+tests_folder = Path(__file__).absolute().parent.parent
+
+
+def test_load_simple_shapes_dataset_vae_local():
+    args = get_args(
+        use_local=False,
+        cli=False,
+        additional_config_files=[
+            tests_folder / "configs/test_base.yaml"
+        ]
+    )
+    load_simple_shapes_dataset(
+        args, args.vae, selected_domains=['v']
+    )
+
+
+def test_load_simple_shapes_dataset_lm_local():
+    args = get_args(
+        use_local=False,
+        cli=False,
+        additional_config_files=[
+            tests_folder / "configs/test_base.yaml"
+        ]
+    )
+    load_simple_shapes_dataset(
+        args, args.lm, add_unimodal=False, selected_domains=['t', 'attr']
+    )
+
+
+def test_load_simple_shapes_dataset_gw_local():
+    args = get_args(
+        use_local=False,
+        cli=False,
+        additional_config_files=[
+            tests_folder / "configs/test_base.yaml"
+        ]
+    )
+    load_simple_shapes_dataset(
+        args, args.global_workspace
+    )
