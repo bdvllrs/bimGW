411610e8ecfbe5521330a55fa3be7cc5412abb9a
porthok <leopold.maytie@gmail.com>
2023-03-22 16:00:23 +0100

    remove one if adding another condition to first one

--- bim_gw/datasets/data_module.py
+++ bim_gw/datasets/data_module.py
@@ -149,10 +149,7 @@ class DataModule(LightningDataModule):
         prop_2_domains = self.prop_labelled_images / self.prop_available_images
         mapping = None
         domain_mapping = None
-        rest = allowed_indices
-        sync_items = allowed_indices
-        n_repeats = 1
-        if prop_2_domains < 1:
+        if (prop_2_domains < 1) or (self.prop_labelled_images < 1):
             original_size = len(allowed_indices * self.prop_available_images)
             labelled_size = int(original_size * prop_2_domains)
             n_repeats = ((len(domains) * original_size) // labelled_size +
@@ -163,7 +160,6 @@ class DataModule(LightningDataModule):
             sync_items, rest = split_indices_prop(
                 permuted_indices, prop_2_domains
             )
-        if self.prop_labelled_images < 1:
             # Add sync
             domain_items = np.tile(sync_items, n_repeats)
             mapping.extend(domain_items)
