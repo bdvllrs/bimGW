055a6911e8591731297708387c28aa8698183adf
Benjamin <bdvllrs@gmail.com>
2023-04-20 15:09:39 +0200

    Pre-saved latent does not require simple_shapes types.

--- bim_gw/datasets/pre_saved_latents.py
+++ bim_gw/datasets/pre_saved_latents.py
@@ -3,13 +3,11 @@ from typing import Dict, List, Optional, SupportsIndex
 
 import numpy as np
 
-from bim_gw.datasets.simple_shapes.types import AvailableDomainsType
-
 
 def load_pre_saved_latent(
     root_path: pathlib.Path, split: str,
-    pre_saved_latent_path: Dict[AvailableDomainsType, str],
-    domain_key: AvailableDomainsType,
+    pre_saved_latent_path: Dict[str, str],
+    domain_key: str,
     ids: Optional[SupportsIndex] = None
 ) -> List[np.ndarray]:
     kept_idx = ids if ids is None else slice(None)
